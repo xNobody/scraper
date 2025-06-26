b08f3c29f32a8a2e5816755c762cfd024a75c485
Catalin Lutescu <lutescudaniel@gmail.com>
2025-06-26 21:31:51 +0300

    Test meta addition for service

--- spec/services/html_scraper_spec.rb
+++ spec/services/html_scraper_spec.rb
@@ -2,26 +2,44 @@ require 'rails_helper'
 
 describe HtmlScraper do
   let(:fixture_path) { Rails.root.join('spec/fixtures/alza_product.html').to_s }
-  let(:fields) do
-    {
-      price: '.price-box__price',
-      rating_count: '.ratingCount',
-      rating_value: '.ratingValue'
-    }
-  end
 
   before do
     html_content = File.read(fixture_path)
     allow(URI).to receive(:open).and_return(StringIO.new(html_content))
   end
 
-  let(:result) { described_class.call(url: fixture_path, fields: fields) }
-
   describe 'scraping from local HTML' do
+    let(:fields) do
+      {
+        price: '.price-box__price',
+        rating_count: '.ratingCount',
+        rating_value: '.ratingValue'
+      }
+    end
+
+    let(:result) { described_class.call(url: fixture_path, fields: fields) }
+
     it 'returns the expected values from the HTML fixture' do
       expect(result[:price]).to eq('18290')
       expect(result[:rating_count]).to eq('7 hodnocení')
       expect(result[:rating_value]).to eq('4,9')
+    end
+  end
+
+  fdescribe 'scraping meta tags' do
+    let(:meta_fields) do
+      {
+        "meta" => [ "keywords", "twitter:image" ]
+      }
+    end
+
+    let(:result) { described_class.call(url: fixture_path, fields: meta_fields) }
+
+    it 'extracts meta tag content by name and property' do
+      expect(result["meta"]).to include(
+        "keywords" => "Parní pračka AEG 7000 ProSteam",
+        "twitter:image" => "https://example.com/image.jpg"
+      )
     end
   end
 end
