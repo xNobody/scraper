c73858ed0e6f84be086ad288108cdc696792dd37
Catalin Lutescu <lutescudaniel@gmail.com>
2025-06-26 09:48:47 +0300

    Fix linting

--- spec/services/html_scraper_spec.rb
+++ spec/services/html_scraper_spec.rb
@@ -1,22 +1,22 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe HtmlScraper do
-  describe ".call" do
-    let(:fixture_path) { Rails.root.join("spec/fixtures/alza_product.html").to_s }
+  describe 'scraping from local HTML' do
+    let(:fixture_path) { Rails.root.join('spec/fixtures/alza_product.html').to_s }
     let(:fields) do
       {
-        price: ".price-box__price",
-        rating_count: ".ratingCount",
-        rating_value: ".ratingValue"
+        price: '.price-box__price',
+        rating_count: '.ratingCount',
+        rating_value: '.ratingValue'
       }
     end
 
     subject(:result) { described_class.call(url: fixture_path, fields: fields) }
 
-    it "returns the expected values from the HTML fixture" do
-      expect(result[:price]).to eq("18290")
-      expect(result[:rating_count]).to eq("7 hodnocení")
-      expect(result[:rating_value]).to eq("4,9")
+    it 'returns the expected values from the HTML fixture' do
+      expect(result[:price]).to eq('18290')
+      expect(result[:rating_count]).to eq('7 hodnocení')
+      expect(result[:rating_value]).to eq('4,9')
     end
   end
 end
