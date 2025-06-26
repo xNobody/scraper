f922d208e02c4cd0a8fa83e76cf2ce24ba5a2f41
Catalin Lutescu <lutescudaniel@gmail.com>
2025-06-26 10:01:13 +0300

    refactor html_scraper test

--- spec/services/html_scraper_spec.rb
+++ spec/services/html_scraper_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe HtmlScraper do
+describe HtmlScraper do
+  let(:fixture_path) { Rails.root.join('spec/fixtures/alza_product.html').to_s }
+  let(:fields) do
+    {
+      price: '.price-box__price',
+      rating_count: '.ratingCount',
+      rating_value: '.ratingValue'
+    }
+  end
+  let(:result) { described_class.call(url: fixture_path, fields: fields) }
+
   describe 'scraping from local HTML' do
-    let(:fixture_path) { Rails.root.join('spec/fixtures/alza_product.html').to_s }
-    let(:fields) do
-      {
-        price: '.price-box__price',
-        rating_count: '.ratingCount',
-        rating_value: '.ratingValue'
-      }
-    end
-
-    subject(:result) { described_class.call(url: fixture_path, fields: fields) }
-
     it 'returns the expected values from the HTML fixture' do
       expect(result[:price]).to eq('18290')
       expect(result[:rating_count]).to eq('7 hodnocení')
